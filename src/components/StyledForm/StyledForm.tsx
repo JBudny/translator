5f65ba75317b74a31a1f6675dfee0c794d912f05
jbudny <32987100+JBudny@users.noreply.github.com>
2024-12-13 17:55:37 +0100

    Add styles for checkbox input

--- src/components/StyledForm/StyledForm.tsx
+++ src/components/StyledForm/StyledForm.tsx
@@ -4,20 +4,21 @@ import {
   forwardRef,
   InputHTMLAttributes,
   OptionHTMLAttributes,
-  SelectHTMLAttributes
+  SelectHTMLAttributes,
 } from "react";
 import {
   StyledFormInputBaseProps,
   StyledFormProps,
-  StyledStyledSelectBaseProps
+  StyledStyledSelectBaseProps,
 } from "./StyledForm.types";
 import {
   StyledBox,
   StyledDistribute,
   StyledDistributeBase,
-  StyledText
-} from '../../../components'
-import { StyledTextBaseCSS } from '../../../components/shared';
+  StyledJustify,
+  StyledText,
+} from "../../../components";
+import { StyledTextBaseCSS } from "../../../components/shared";
 
 const StyledFormInputBase = styled.input<StyledFormInputBaseProps>`
   ${StyledTextBaseCSS};
@@ -27,8 +28,42 @@ const StyledFormInputBase = styled.input<StyledFormInputBaseProps>`
   border-bottom: ${({ theme }) => `1px solid rgb(${theme.palette.gray100})`};
 
   &::placeholder {
-    color: ${({ theme }) => `rgb(${theme.palette.gray300})`}; 
-  };
+    color: ${({ theme }) => `rgb(${theme.palette.gray300})`};
+  }
+`;
+
+const StyledFormCheckboxBase = styled.div`
+  padding: ${({ theme }) => theme.tokens.spacing2};
+  background-color: ${({ theme }) => `rgb(${theme.palette.transparent})`};
+  border: ${({ theme }) =>
+    `1px solid rgba(${theme.palette.gray100}, ${theme.tokens.opacity2})`};
+  border-radius: ${({ theme }) => theme.tokens.borderRadius1};
+  position: relative;
+
+  &::before {
+    position: absolute;
+    inset: 3px;
+    content: "";
+    transition: opacity 0.23s cubic-bezier(1, 0, 0.04, 1);
+    border-radius: 3px;
+    background-color: ${({ theme }) => `rgb(${theme.palette.gray100})`};
+    opacity: 0;
+  }
+`;
+
+const StyledFormInputCheckboxBase = styled.input.attrs({
+  type: "checkbox",
+})`
+  display: none;
+
+  &:checked ~ ${StyledFormCheckboxBase} {
+    border: ${({ theme }) =>
+    `1px solid rgba(${theme.palette.gray100}, ${theme.tokens.opacity1})`};
+
+    &::before {
+      opacity: 1;
+    }
+  }
 `;
 
 const StyledSelectBase = styled.select<StyledStyledSelectBaseProps>`
@@ -45,9 +80,13 @@ const StyledOptionBase = styled.option`
 `;
 
 export const StyledForm: StyledFormProps = ({ children, ...props }) => {
-
   return (
-    <StyledDistributeBase as='form' $direction="column" $gap="spacing3" {...props}>
+    <StyledDistributeBase
+      as="form"
+      $direction="column"
+      $gap="spacing3"
+      {...props}
+    >
       {children}
     </StyledDistributeBase>
   );
@@ -60,7 +99,12 @@ const StyledFormField: StyledFormProps["Field"] = ({
   error,
   ...props
 }) => (
-  <StyledBox background="gray500" padding="spacing2" rounding="borderRadius2" {...props}>
+  <StyledBox
+    background="gray500"
+    padding="spacing2"
+    rounding="borderRadius2"
+    {...props}
+  >
     <StyledDistribute gap="spacing2">
       <label htmlFor={htmlFor}>
         <StyledText $size="medium" $weight="normal" as="span" $color="gray100">
@@ -81,32 +125,37 @@ const StyledSelect = forwardRef<
   HTMLSelectElement,
   SelectHTMLAttributes<HTMLSelectElement>
 >(({ children, ...props }, ref) => (
-  <StyledSelectBase
-    $size="small"
-    $weight="normal"
-    ref={ref}
-    {...props}
-  >
+  <StyledSelectBase $size="small" $weight="normal" ref={ref} {...props}>
     {children}
   </StyledSelectBase>
 ));
 
-const StyledOption: FC<OptionHTMLAttributes<HTMLOptionElement>> = ({ children, ...props }) => {
-  return (<StyledOptionBase {...props}>{children}</StyledOptionBase>);
+const StyledOption: FC<OptionHTMLAttributes<HTMLOptionElement>> = ({
+  children,
+  ...props
+}) => {
+  return <StyledOptionBase {...props}>{children}</StyledOptionBase>;
 };
 
 const FormInput = forwardRef<
   HTMLInputElement,
   InputHTMLAttributes<HTMLInputElement>
->(({ className, type, ...props }, ref) => (
-  <StyledFormInputBase
-    $size="small"
-    $weight="normal"
-    ref={ref}
-    type={type}
-    {...props}
-  />
-));
+>(({ className, type, ...props }, ref) =>
+  type === "checkbox" ? (
+    <StyledJustify justify="flex-start">
+      <StyledFormInputCheckboxBase ref={ref} type={type} {...props} />
+      <StyledFormCheckboxBase />
+    </StyledJustify>
+  ) : (
+    <StyledFormInputBase
+      $size="small"
+      $weight="normal"
+      ref={ref}
+      type={type}
+      {...props}
+    />
+  )
+);
 
 StyledForm.Field = StyledFormField;
 StyledForm.Input = FormInput;
